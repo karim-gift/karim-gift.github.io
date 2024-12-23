afaa34accf48be6516b75877e65147aa32152e12
Karim Hajy <karim.hajy@mirakl.com>
2024-12-23 02:15:26 +0100

    ajout de liens pour chaque spectacle dans le fichier shows.ts

--- src/data/shows.ts
+++ src/data/shows.ts
@@ -3,18 +3,21 @@ export const shows = [
     title: "L'Heure des assassins",
     synopsis: "Après le succès de \"Le Cercle de Whitechapel\" et \"Les Voyageurs du crime\", une nouvelle comédie policière riche en rebondissements.",
     youtube: "https://www.youtube.com/watch?v=JKH0A32d8jM",
-    image: "https://www.billetreduc.com/f700-800-0/vz-4052cd31-3ab7-4c9e-bd0d-10d613349485.jpeg"
+    image: "https://www.billetreduc.com/f700-800-0/vz-4052cd31-3ab7-4c9e-bd0d-10d613349485.jpeg",
+    link: "https://www.billetreduc.com/351604/evt.htm"
   },
   {
     title: "Adn",
     synopsis: "Thriller. À la suite d'un test ADN, Tomas découvre qu'il n'est pas le père de son bébé - mais son oncle. Seulement, à sa connaissance, il n'a pas de frère.",
     youtube: "https://www.youtube.com/watch?v=M-Vqf0VY39I",
-    image: "https://www.billetreduc.com/f700-800-0/vz-49fd9707-1107-4c49-b064-c5cdc9bc950c.jpeg"
+    image: "https://www.billetreduc.com/f700-800-0/vz-49fd9707-1107-4c49-b064-c5cdc9bc950c.jpeg",
+    link: "https://www.billetreduc.com/347202/evt.htm"
   },
   {
     title: "La Claque",
     synopsis: "Drôle, musical et interactif, l'incroyable histoire d'un chef d'orchestre d'applaudissements.",
     youtube: "https://www.youtube.com/watch?v=i8u2BBycZyc",
-    image: "https://www.billetreduc.com/f700-800-0/vz-cae0e390-bfe9-4844-9735-98eb5aa35e5b.jpeg"
+    image: "https://www.billetreduc.com/f700-800-0/vz-cae0e390-bfe9-4844-9735-98eb5aa35e5b.jpeg",
+    link: "https://www.billetreduc.com/293581/evt.htm"
   }
 ];
