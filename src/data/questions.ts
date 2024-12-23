38f7cf83ac457601da861b08d9232ff8d526185b
Karim Hajy <karim.hajy@mirakl.com>
2024-12-23 12:23:24 +0100

    Mise à jour des questions du quiz avec de nouveaux textes, images et réponses, ainsi que modification du chemin du script dans index.html

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1,23 +1,37 @@
 export const questions = [
   {
     idPage: 1,
-    text: "Déchiffrez le code mystérieux de Sherlock",
+    text: "Adresse du détective",
     imageUrl: "https://images.unsplash.com/photo-1524668951403-d44b28200ce0?w=800&auto=format&fit=crop",
     mp3Url: "/audio/Question1.mp3",
     answer: "221B"
   },
   {
     idPage: 2,
-    text: "Trouvez la séquence ADN cachée",
-    imageUrl: "https://images.unsplash.com/photo-1530026405186-ed1f139313f8?w=800&auto=format&fit=crop",
+    text: "Objet",
+    imageUrl: "https://i.ibb.co/bX1qq23/DALL-E-2024-12-23-04-05-48-A-highly-detailed-illustration-of-a-Victorian-era-detective-s-study-drawn.webp",
     mp3Url: "/audio/Question2.mp3",
-    answer: "ATGC"
+    answer: "VIOLON"
   },
   {
     idPage: 3,
-    text: "Quel est le code final?",
-    imageUrl: "https://images.unsplash.com/photo-1482517967863-00e15c9b44be?w=800&auto=format&fit=crop",
+    text: "Nom",
+    imageUrl: "https://i.ibb.co/RzZFQhh/Capture-d-e-cran-2024-12-23-a-05-31-18.png",
     mp3Url: "/audio/Question3.mp3",
-    answer: "NOEL"
-  }
+    answer: "BLAKE"
+  },
+  {
+    idPage: 4,
+    text: "Code",
+    imageUrl: "https://i.ibb.co/KNyTRGn/DALL-E-2024-12-23-06-02-03-A-dark-and-mysterious-laboratory-scene-illuminated-by-dim-eerie-lights-Th.webp",
+    mp3Url: "/audio/Question4.mp3",
+    answer: "ATGCX"
+  },
+  {
+    idPage: 5,
+    text: "L'odyssée de Balbuzar",
+    imageUrl: "https://i.ibb.co/VvH2hQ8/DALL-E-2024-12-23-11-56-35-A-mysterious-island-at-night-under-a-stormy-sky-with-flashes-of-lightning.webp",
+    mp3Url: "/audio/Question5.mp3",
+    answer: "13"
+  },
 ];
